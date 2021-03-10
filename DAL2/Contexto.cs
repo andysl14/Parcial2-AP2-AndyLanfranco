8c3e4e00cbcf0ff20a547b8862161cae19103093
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-03-10 18:06:59 -0400

    Creando DAL 2

---
+++ DAL2/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_AP2_AndyLanfranco.DAL2
+{
+    public class Contexto
+    {
+    }
+}
