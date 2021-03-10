669ffd248b5bf42aba73a3c9b9d26073f867f1dc
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-03-09 22:32:49 -0400

    Creando las Entidades Clientes y Ventas

---
+++ Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2_AP2_AndyLanfranco.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
